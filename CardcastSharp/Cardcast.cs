b1db55392ebe3f20c37fd7f72c299bd710be64a8
Brosilio <brosilio@youshouldfear.me>
2019-10-28 01:42:46 -0400

    Update Cardcast.cs
    Changed some things. All additions untested for reliability. New helper class for caching decks. (untested as is)

--- CardcastSharp/Cardcast.cs
+++ CardcastSharp/Cardcast.cs
@@ -24,50 +24,126 @@ namespace CardcastSharp
         /// </summary>
         public const string API_CARDS = "/cards";
 
-        private static WebClient webClient = new WebClient();
+        /// <summary>
+        /// The WebClient used internally to get stuff.
+        /// </summary>
+        private static readonly WebClient webClient = new WebClient();
 
         /// <summary>
-        /// Returns a deck of cards by code
+        /// Returns a deck of cards by code. Will throw exception if something goes wrong.
         /// </summary>
         /// <param name="code">The Cardcast deck play code</param>
-        /// <param name="onErrorCallback">Call this method if there is some error while obtaining the deck</param>
-        /// <returns>Returns null if the deck couldn't be found or there was some fucking shit error while trying to get it</returns>
-        public static async Task<Deck> GetDeck(string code, Action<Exception> onErrorCallback = null)
+        public static async Task<Deck> GetDeck(string code)
         {
-            try
-            {
-                string deckJson = await webClient.DownloadStringTaskAsync($"{API_ENDPOINT}{code}{API_CARDS}");
-                Deck temp = JsonConvert.DeserializeObject<Deck>(deckJson);
-                temp.Info = await GetDeckInformation(code, onErrorCallback);
-                return temp;
-            }
-            catch(Exception ex)
-            {
-                onErrorCallback?.Invoke(ex);
-                return null;
-            }
+            string deckJson = await webClient.DownloadStringTaskAsync($"{API_ENDPOINT}{code}{API_CARDS}");
+            Deck temp = JsonConvert.DeserializeObject<Deck>(deckJson);
+            temp.Info = await GetDeckInformation(code);
+            return temp;
         }
 
         /// <summary>
-        /// Returns all the information about a deck.
+        /// Returns all the information about a deck. Will throw exception if something goes wrong.
         /// </summary>
         /// <param name="code">The playcode of the deck.</param>
-        /// <returns>Returns the deck info. Returns null if it couldn't be found.</returns>
-        public static async Task<DeckInfo> GetDeckInformation(string code, Action<Exception> onErrorCallback = null)
+        public static async Task<DeckInfo> GetDeckInformation(string code)
         {
-            try
-            {
-                string deckInfoJson = await webClient.DownloadStringTaskAsync($"{API_ENDPOINT}{code}");
-                return JsonConvert.DeserializeObject<DeckInfo>(deckInfoJson);
-            }
-            catch (Exception ex)
-            {
-                onErrorCallback?.Invoke(ex);
-                return null;
-            }
+            string deckInfoJson = await webClient.DownloadStringTaskAsync($"{API_ENDPOINT}{code}");
+            return JsonConvert.DeserializeObject<DeckInfo>(deckInfoJson);
         }
     }
 
+    /// <summary>
+    /// A helper class for caching decks in memory.
+    /// Create a new one of these with the required information, then use GetDeck on it.
+    /// It will either try to get the deck from the server if it isn't cached, or return
+    /// the cached version.
+    /// </summary>
+    public class CardcastCache
+    {
+        /// <summary>
+        /// Helper class for stuff that needs to be done.
+        /// </summary>
+        private class DeckCacheObject
+        {
+            public DateTime cacheTime;
+            public Deck cachedDeck;
+
+            public DeckCacheObject(Deck deck)
+            {
+                this.cacheTime = DateTime.Now;
+                this.cachedDeck = deck;
+            }
+        }
+
+        private readonly Dictionary<string, DeckCacheObject> cachedDecks = new Dictionary<string, DeckCacheObject>();
+
+        /// <summary>
+        /// The time-to-live for cached decks, in seconds. If they've gone foul, they will be redownloaded.
+        /// </summary>
+        public float TTL { get; set; }
+
+        /// <summary>
+        /// New Cardcast Cache.
+        /// </summary>
+        /// <param name="ttl">The time to live of cached decks, in seconds.</param>
+        public CardcastCache(float ttl)
+        {
+            this.TTL = ttl;
+        }
+
+        /// <summary>
+        /// Gets a deck using this CardcastCache's cache (if it has it.)
+        /// Otherwise, it downloads it, then caches it.
+        /// Will throw if angered or provoked.
+        /// </summary>
+        /// <param name="playCode">The playcode of the deck to get.</param>
+        /// <returns></returns>
+        public async Task<Deck> GetDeck(string playCode)
+        {
+            playCode = playCode.ToLower().Trim();
+
+            if (this.cachedDecks.ContainsKey(playCode))
+            {
+                if (DateTime.Now.Subtract(this.cachedDecks[playCode].cacheTime).TotalSeconds < this.TTL)
+                {
+                    return this.cachedDecks[playCode].cachedDeck;
+                }
+                else
+                {
+                    this.cachedDecks[playCode] = new DeckCacheObject(await Cardcast.GetDeck(playCode));
+                    return this.cachedDecks[playCode].cachedDeck;
+                }
+            }
+            else
+            {
+                this.cachedDecks.Add(playCode, new DeckCacheObject(await Cardcast.GetDeck(playCode)));
+                return this.cachedDecks[playCode].cachedDeck;
+            }
+        }
+
+        /// <summary>
+        /// Expunges a cached deck from the cached decks.
+        /// </summary>
+        /// <param name="playCode">The playcode of the deck to expunge.</param>
+        /// <returns>Returns true if the deck was expunged, false if not.</returns>
+        public bool Expunge(string playCode)
+        {
+            playCode = playCode.ToLower().Trim();
+            if (this.cachedDecks.ContainsKey(playCode))
+            {
+                this.cachedDecks.Remove(playCode);
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+
+
+    /// <summary>
+    /// Contains information about decks.
+    /// </summary>
     public class DeckInfo
     {
         /// <summary>
